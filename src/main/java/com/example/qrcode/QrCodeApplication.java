e2580c52fcdb9f84dd112b9d897e0fcb23621fe6
Manas Agarwal <lcs2020021@iiitl.ac.in>
2024-07-02 21:33:19 +0530

    add print statement

--- src/main/java/com/example/qrcode/QrCodeApplication.java
+++ src/main/java/com/example/qrcode/QrCodeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QrCodeApplication {
 
     public static void main(String[] args) {
+        System.out.println("The application is running");
         SpringApplication.run(QrCodeApplication.class, args);
     }
 
